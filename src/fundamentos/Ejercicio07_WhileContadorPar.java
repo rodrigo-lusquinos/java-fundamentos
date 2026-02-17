e8f73f31d7e0ac9f36cfb92007bb7a285bddb6f4
Rodrigo Lusquiños <lusquinosrodrigo@gmail.com>
2026-02-17 17:21:35 -0300

    Ejercicio 7 - Utilización de while para contador par

---
+++ src/fundamentos/Ejercicio07_WhileContadorPar.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fundamentos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lusqu
+ */
+public class Ejercicio07_WhileContadorPar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num;
+        
+        System.out.println("Ingresá un número positivo: ");
+        num = Integer.parseInt(input.nextLine());
+        
+        if (num > 0) {
+            
+            if (num % 2 != 0) {
+                num--; // es lo mismo que num = num - 1;
+            }
+            
+            int cont = num;
+            
+            while (cont >= 0) {                
+                System.out.print(cont + " ");
+                cont -= 2; //es lo mismo que usar cont = cont - 2;
+            }
+            System.out.println();
+        } else {
+            System.out.println("El número debía ser positivo");
+        }
+    }
+}
