fbb0467ca9720fff29cbde38f04db11f4034edfe
Rodrigo Lusquiños <lusquinosrodrigo@gmail.com>
2026-02-23 16:31:46 -0300

    Corrección de ejercicio 9

--- src/fundamentos/Ejercicio09_Edades.java
+++ src/fundamentos/Ejercicio09_Edades.java
@@ -8,32 +8,40 @@ import java.util.Scanner;
 
 /**
  *
- * @author lusqu
+ * @author lusquiños
  */
 public class Ejercicio09_Edades {
+
     private static final int EDAD_MINIMA = 18;
-    
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int cantPersonas, edad, contMayoresDeEdad = 0;
         double porcMayoresDeEdad;
-        
+
         System.out.println("Ingresá la cantidad de personas: ");
         cantPersonas = Integer.parseInt(input.nextLine());
-        
-        for (int i = 1; i <= cantPersonas ; i++) {
-            System.out.println("Ingresa la edad de la persona n° " + i);
-            edad= Integer.parseInt(input.nextLine());
-            if (edad >= EDAD_MINIMA) {
-                contMayoresDeEdad++;
-                System.out.println("");
+
+        if (cantPersonas > 0) {
+
+            for (int i = 1; i <= cantPersonas; i++) {
+                System.out.println("Ingresa la edad de la persona n° " + i);
+                edad = Integer.parseInt(input.nextLine());
+
+                while (edad <= 0) {
+                    System.out.println("Ingresar edad mayor a 0 años");
+                    System.out.println("Ingresa la edad de la persona n° " + i);
+                    edad = Integer.parseInt(input.nextLine());
+                }
+
+                if (edad >= EDAD_MINIMA) {
+                    contMayoresDeEdad++;
+                }
             }
+            porcMayoresDeEdad = (contMayoresDeEdad * 100.0) / cantPersonas;
+            System.out.println(porcMayoresDeEdad + "% de personas fueron mayores de " + EDAD_MINIMA);
+        } else {
+            System.out.println("La cantidad de personas debe ser mayor a 0");
         }
-        
-        porcMayoresDeEdad = (contMayoresDeEdad * 100.0) / cantPersonas;
-        System.out.println(porcMayoresDeEdad + "% de personas fueron mayores de " + EDAD_MINIMA);
-        
-    
-    
-    }    
+    }
 }
